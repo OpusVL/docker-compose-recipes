2d8356ec82a549de9d8bac145093c962f2b24b3d
Kane Valentine <kane@cute.im>
2017-11-10 14:31:29 +0000

    uploaded webtrees recipe

---
+++ docker-webtrees/webtrees/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -euo pipefail
+
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+if ! [ -e index.php ]; then
+	echo >&2 "Webtrees not found in $PWD - copying now..."
+	if [ "$(ls -A)" ]; then
+		echo >&2 "WARNING: $PWD is not empty - press Ctrl+C now if this is an error!"
+		( set -x; ls -A; sleep 10 )
+	fi
+	tar cf - --one-file-system -C /usr/src/webtrees-${WEBTREES_VERSION} . | tar xf -
+	echo >&2 "Complete! Webtrees has been successfully copied to $PWD"
+fi
+
+exec "$@"
