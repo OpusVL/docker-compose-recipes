da17c878520ccda532a26f3148333552c74a1113
Kane Valentine <kane@cute.im>
2017-11-15 11:54:52 +0000

    first alpha version of osTicket, DO NOT USE

---
+++ docker-osticket/osticket/docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -euo pipefail
+
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+if ! [ -e index.php ]; then
+	echo >&2 "osTicket not found in $PWD - copying now..."
+	if [ "$(ls -A)" ]; then
+		echo >&2 "WARNING: $PWD is not empty - press Ctrl+C now if this is an error!"
+		( set -x; ls -A; sleep 10 )
+	fi
+	tar cf - --one-file-system -C /usr/src/osTicket-${OSTICKET_VERSION} . | tar xf -
+	echo >&2 "Complete! osTicket has been successfully copied to $PWD"
+fi
+
+cp include/ost-sampleconfig.php include/ost-config.php
+chmod 0666 include/ost-config.php
+
+exec "$@"
